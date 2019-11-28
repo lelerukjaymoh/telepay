40140cd60320521b5b780e79d8804f636c9991f2
coderjaymoh <lelerukjaymoh@gmail.com>
2019-11-28 05:34:03 +0300

    cleaned forms.py

--- telegramlipa/forms.py
+++ telegramlipa/forms.py
@@ -2,19 +2,6 @@ from django import forms
 from telegramlipa.models import Profile 
 
 
-# class MyModelForm(forms.ModelForm):
-#    def __init__(self, *args, **kwargs):
-#        self.request = kwargs.pop('request', None)
-#        return super().__init__(*args, **kwargs)
-
-#    def save(self, *args, **kwargs):
-#        kwargs['commit'] = False
-#        obj = super(MyModelForm, self).save(*args, **kwargs)
-#        if self.request:
-#            obj.user = self.request.user
-#        obj.save()
-#        return obj
-
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
